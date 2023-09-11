f3227188346b186cb363725fcc93ebd67955b766
Omozusi Guobadia <65314173+mguobadia@users.noreply.github.com>
2023-09-11 19:17:35 -0400

    Update TimelineData.js

--- src/components/About/TimelineData.js
+++ src/components/About/TimelineData.js
@@ -1,58 +1,75 @@
 const TimelineItems = [
-  {
-    title: "MIT OME",
-    cardTitle: "Undergraduate Teaching Assistant",
-    cardSubtitle: `MIT OME Momentum (2020)`,
-    cardDetailedText: `Served as a Course Assistant for a project-oriented course given to current undergraduates by Lockheed Martin, which entails designing guidance algorithms to react to stimuli (such as ground terrain) in order to fly a drone autonomously in a simulated environment. Conducted meetings regularly with employees from the company to address issues with the guidance software.`
-  },
+ 
+  
     {
-      title: "SIGNIFY",
-      cardTitle: "Hardware Engineering Intern",
-      cardSubtitle:
-        "Signify R&D Team (2021)",
-      cardDetailedText: `Optimized firmware for the Philips Hue Home System by utilizing Raspberry Pi as a model for testing and operating the Yocto-based operating software capable of conducting routine remote updates to the Phillips Hue Lighting System.`
+      title: "MIT Bioelectronics Laboratory & Jasanoff Laboratory",
+      cardTitle: "Graduate Researcher",
+      cardSubtitle: `Bioelectronics Laboraory & Jasanoff Laboratory (2023-)`,
+      cardDetailedText: `Conducted research in multifunctional fiber development for utilization in DBS models in rats; Created and soldered 20+ fiber electrodes; Conducted rat brain slicing; Monitored and handled rats and mice in in-vivo neurosurgery.`
     },
+
+    
+    
+  
     {
-      title: "MIT PILM",
+      title: "PILM w/ UCL",
       cardTitle: "Undergraduate Research Assistant",
-      cardSubtitle: `MIT Picower Institute for Learning and Memory (2021)`,
-      cardDetailedText: `Conducted in-depth research to elucidate key brain regions contributing to learning in mice using cutting-edge optogenetics techniques.
-      Collaborated with a multidisciplinary team to contribute to the design and refinement of a state-of-the-art VR simulation device.
-      Played an integral role in the development phase, leveraging MATLAB-run software to enhance the efficiency of trial execution and data collection.
-      Demonstrated a proactive and hands-on approach, working towards accelerating the pace of experimentation through innovative technological solutions.`
+      cardSubtitle: `MIT Picower Institute of Learning and Memory w/ UCL (2022)`,
+      cardDetailedText: `Programmed a lasso-regression generalized linear model, and trained a binomial classifier on MEG Data provided by University of College, London researchers; Analyzed previously constructed Matlab scripts utilized to run similar experiments, as well as papers by previous studies on human memory replay; Implemented, commented, and decreased runtime of Matlab scripts used for data processing; Presented progress through weekly PowerPoints that represented the MEG data post-processed with different abscissa quantities.`
     },
-    {
-      title: "MIT MEDIA LAB",
-      cardTitle: "Undergraduate Research Assistant/Hardware Engineering Intern",
-      cardSubtitle: `Motorola Group @ MIT Media Lab (2021-2022)`,
-      cardDetailedText: `Conducted research and worked with a team of undergraduate researchers to develop the AttentivU Glasses; Aided physical configuration (through soldering, CAD, and EVT), firmware development, and overall debugging of other HCI peripherals that collected EEG readings through mastoid and front polar locations; Reviewed schematics and assembled various HCI devices including a set of EEG Headphones and Glasses; Aided testing of HCI devices and redrawing of schematics for said devices.`
-    },
-    {
-      title: "IBM",
-      cardTitle: "Hardware Engineering Intern",
-      cardSubtitle:
-        "IBM (2022)",
-      cardDetailedText: `Designed IC Timers, SOICs, DIPs, Temperature Sensors, and various other hardware components/circuits that are used in industry-ready cards included in IBM Z Metis Mainframe. Presented designs to a panel of PCB design engineers, as well as the Vice President of Physical Design Team. Aided in the creation of a component library to store designed components.`
-    },
+
+    
+  
+  
     {
       title: "NSLC",
       cardTitle: "Engineering Team Advisor",
       cardSubtitle: `National Student Leadership Conference (2022)`,
       cardDetailedText: `Worked as a team advisor for two sessions; Guided groups of 12-14 high school students through various engineering and leadership activities; Each group designed their own RC car, Arduino-coded Line Following Car, Aircraft Carrier Launch and Water Rover while also expanding their interpersonal skills with sessions of leadership activities.`
     },
-    {
-      title: "MIT PILM w/ UCL",
-      cardTitle: "Undergraduate Research Assistant",
-      cardSubtitle: `MIT Picower Institute of Learning and Memory w/ UCL (2022)`,
-      cardDetailedText: `Programmed a lasso-regression generalized linear model, and trained a binomial classifier on MEG Data provided by University of College, London researchers; Analyzed previously constructed Matlab scripts utilized to run similar experiments, as well as papers by previous studies on human memory replay; Implemented, commented, and decreased runtime of Matlab scripts used for data processing; Presented progress through weekly PowerPoints that represented the MEG data post-processed with different abscissa quantities.`
-    },
+
     
     {
-      title: "MIT Bioelectronics Laboratory & Jasanoff Laboratory",
-      cardTitle: "Graduate Researcher",
-      cardSubtitle: `Bioelectronics Laboraory & Jasanoff Laboratory (2023-)`,
-      cardDetailedText: `Currently conducting research in multifunctional fiber development as part of two research labs`
+      title: "IBM",
+      cardTitle: "Hardware Engineering Intern",
+      cardSubtitle:
+        "IBM (2022)",
+      cardDetailedText: `Designed IC Timers, SOICs, DIPs, Temperature Sensors, and various other hardware components/circuits that are used in industry-ready cards included in IBM Z Metis Mainframe; Presented designs to a panel of PCB design engineers, as well as the Vice President of Physical Design Team; Aided in the creation of a component library to store designed components.`
     },
+
+    
+    
+    {
+      title: "MEDIA LAB",
+      cardTitle: "Undergraduate Research Assistant/Hardware Engineering Intern",
+      cardSubtitle: `Motorola Group @ MIT Media Lab (2021-2022)`,
+      cardDetailedText: `Conducted research and worked with a team of undergraduate researchers to develop the AttentivU Glasses; Aided physical configuration (through soldering, CAD, and EVT), firmware development, and overall debugging of other HCI peripherals that collected EEG readings through mastoid and front polar locations; Reviewed schematics and assembled various HCI devices including a set of EEG Headphones and Glasses; Aided testing of HCI devices and redrawing of schematics for said devices.`
+    },
+  {
+      title: "PILM",
+      cardTitle: "Undergraduate Research Assistant",
+      cardSubtitle: `MIT Picower Institute for Learning and Memory (2021)`,
+      cardDetailedText: `Conducted in-depth research to elucidate key brain regions contributing to learning in mice using cutting-edge optogenetics techniques;
+      Collaborated with a multidisciplinary team to contribute to the design and refinement of a state-of-the-art VR simulation device;
+      Played an integral role in the development phase, leveraging MATLAB-run software to enhance the efficiency of trial execution and data collection;
+      Demonstrated a proactive and hands-on approach, working towards accelerating the pace of experimentation through innovative technological solutions.`
+    },
+    
+  
+  {
+      title: "SIGNIFY",
+      cardTitle: "Hardware Engineering Intern",
+      cardSubtitle:
+        "Signify R&D Team (2021)",
+      cardDetailedText: `Optimized firmware for the Philips Hue Home System by utilizing Raspberry Pi as a model for testing and operating the Yocto-based operating software capable of conducting routine remote updates to the Phillips Hue Lighting System.`
+    },
+  
+  {
+    title: "OME",
+    cardTitle: "Undergraduate Teaching Assistant",
+    cardSubtitle: `MIT OME Momentum (2020)`,
+    cardDetailedText: `Served as a Course Assistant for a project-oriented course given to current undergraduates by Lockheed Martin, which entails designing guidance algorithms to react to stimuli (such as ground terrain) in order to fly a drone autonomously in a simulated environment; Conducted meetings regularly with employees from the company to address issues with the guidance software.`
+  },
     
   ];
   
